c0996701c3c38af65065198d53cb53d3091179ef
slemanz <william_sleman@hotmail.com>
2025-03-13 23:29:35 -0300

    billing(feat): begin add item fn

--- market_billing_system/main.cpp
+++ market_billing_system/main.cpp
@@ -43,6 +43,48 @@ class Bill
         }
 };
 
+void addItem(Bill b)
+{
+    bool close = false;
+    while(!close)
+    {
+        int choice;
+        cout << "\t1. Add" << endl;
+        cout << "\t2. Close" << endl;
+        cout << "\tEnter Choice: " << endl;
+        cin  >> choice;
+
+        if(choice == 1)
+        {
+            string item;
+            int rate, quant;
+
+            cout << "\tEnter Item Name: ";
+            cin  >> item;
+            b.setItem(item);
+
+            cout << "\tEnter Rate of Item: ";
+            cin  >> rate;
+            b.setRate(rate);
+
+            cout << "\tEnter Quantity of Item: ";
+            cin >> rate;
+            b.setQuant(quant);
+
+            ofstream out("../Bill.txt", ios::app); // append mode
+            if(!out)
+            {
+                cout << "\tError: File Can't Open!" << endl;
+            }else
+            {
+                out << "\t" << b.getItem() << " : " << b.getRate() << " : " << b.getQuant() <<endl << endl;
+            }
+            out.close();
+            cout << "\tItem Added Successfuly" << endl;
+        }
+    }
+}
+
 int main()
 {
     Bill b;
@@ -66,7 +108,7 @@ int main()
 
         if(val == 1)
         {
-            
+
         }
 
         if(val == 3)
